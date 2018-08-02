a881e77346a47b8c39b97612ec6895cf97c07be0
Elle Faraday <faraday.elle@gmail.com>
2018-08-02 19:10:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
+  before_action :current_user
+  skip_before_action :current_user, only: [:new, :create]
 
+  def new
+  end
+
+  def create
+  end
   private
   def current_user
     redirect_to '/login' unless session.include? :username
+  end
 end
